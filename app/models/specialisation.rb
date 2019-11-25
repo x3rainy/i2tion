1a2c441e26c6d59b5ac2eedcbf5f4e087d3c5777
Zache He <zet_he@hotmail.com>
2019-11-25 18:33:31 +0800

    adjusted some formatting

--- app/models/specialisation.rb
+++ app/models/specialisation.rb
@@ -1,23 +1,24 @@
 class Specialisation < ApplicationRecord
-  validates :name, presence: true, inclusion: { in: ['Mathematics',
-    'Algebra',
-    'Geometry',
-    'Science',
-    'Geography',
-    'History',
-    'English',
-    'Spanish',
-    'German',
-    'French',
-    'Latin',
-    'Greek',
-    'Arabic',
-    'Computer Science',
-    'Art',
-    'Economics',
-    'Music',
-    'Drama',
-    'Physical Education'] }
   has_many :tutor_specialisations
   has_many :tutors, through: :tutor_specialisations
+
+  validates :name, presence: true, inclusion: { in: ['Mathematics',
+  'Algebra',
+  'Geometry',
+  'Science',
+  'Geography',
+  'History',
+  'English',
+  'Spanish',
+  'German',
+  'French',
+  'Latin',
+  'Greek',
+  'Arabic',
+  'Computer Science',
+  'Art',
+  'Economics',
+  'Music',
+  'Drama',
+  'Physical Education'] }
 end
--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,12 +1,10 @@
 class Tutor < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
+
   has_many :tutor_specialisations
   has_many :specialisations, through: :tutor_specialisations
-=======
   has_many :bookings
   has_many :reviews, through: :bookings
->>>>>>> master
 
   validates :age, presence: true
   validates :gender, presence: true
