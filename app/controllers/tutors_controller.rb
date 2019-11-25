e67a905cb88e9018d7973f58c727d8a73db0df15
Zache He <zet_he@hotmail.com>
2019-11-25 22:06:35 +0800

    created tutors controller,actions, routes, and views

--- app/controllers/tutors_controller.rb
+++ app/controllers/tutors_controller.rb
@@ -1,22 +1,46 @@
 class TutorsController < ApplicationController
+  before_action :set_tutor, only: %i[show edit update destroy]
+
   def index
+    @tutors = Tutor.all
   end
 
   def show
+    # @tutor_specialisation = TutorSpecialisation.new
   end
 
   def new
+    @tutor = Tutor.new
   end
 
   def create
+    @tutor = Tutor.new(tutor_params)
+    if @tutor.save!
+      redirect_to tutors_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    redirect_to tutor_path(@tutor) if @tutor.update(tutor_params)
   end
 
   def destroy
+    @tutor.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_tutor
+    @tutor = Tutor.find(params[:id])
+  end
+
+  def tutor_params
+    params.require(:tutor).permit(:age, :gender, :bio, :activity_status)
   end
 end
