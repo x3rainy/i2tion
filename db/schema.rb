4a1d5f40cfbde386a1cb0ee7c3bdf2aa14da2549
Nesh Hari <shrineshharips4@gmail.com>
2019-11-25 18:10:50 +0800

    created routes and controller for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_11_25_085931) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "specialisations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-=======
+  end
+
   create_table "tutors", force: :cascade do |t|
     t.integer "age"
     t.string "gender"
@@ -32,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_11_25_085931) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_tutors_on_user_id"
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
