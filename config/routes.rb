92b1733dcb9bae0482bd478ef8593745ce000b08
Zache He <zet_he@hotmail.com>
2019-11-26 16:10:09 +0800

    --no-edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, except: [:new, :create, :index, :delete]
-  resources :tutors
+  resources :users, except: [:new, :create, :index, :delete] do
+    resources :bookings, except: [:new, :create]
+  end
+
+  resources :tutors do
+    resources :bookings
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
