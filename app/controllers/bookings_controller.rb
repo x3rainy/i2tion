19b3c57f624f754b8a54e33bf7641f6cf9a6c0c5
Nesh Hari <shrineshharips4@gmail.com>
2019-11-29 15:08:35 +0800

    map popups

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,7 +15,9 @@ class BookingsController < ApplicationController
     @markers = @bookings.map do |booking|
       {
         lat: booking.latitude,
-        lng: booking.longitude
+        lng: booking.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { booking: booking }),
+        image_url: helpers.asset_url('map1.png')
       }
     end
   end
